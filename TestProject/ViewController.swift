fcd811bfc0d8c976ea7ddf7a99dc1bb2f5569ec8
Ian Oxborrow <ian.oxborrow@cgs.act.edu.au>
2015-07-21 12:48:17 +1000

    Initial commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Ian Oxborrow on 21/07/2015.
+//  Copyright © 2015 Flawless Logic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
