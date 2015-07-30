f8f158322b40ca4d80a568a89df31e4d6630ae0d
Ian Oxborrow <ian.oxborrow@cgs.act.edu.au>
2015-07-30 15:08:16 +1000

    chagning checkout

---
+++ SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  TestProject
+//
+//  Created by Ian Oxborrow on 30/07/2015.
+//  Copyright © 2015 Flawless Logic. All rights reserved.
+//
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //Created new viewcontroller file
+        print("view loaded")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
